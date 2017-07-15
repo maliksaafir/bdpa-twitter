358f112c3622b222b338af7edf04a8ab61bf12eb
christophrrb <christopherballenger@gmail.com>
2017-07-15 13:21:19 -0400

    tweet_details is for a Tweet that's clicked on, and tweet_card is the card that's going to be queried every time a Tweet is from the database.

--- tweet_card.php
+++ tweet_card.php
@@ -1,11 +1,4 @@
-<html>
-<head>
-  <title>Tweet</title>
-  <link rel="stylesheet" type="text/css" href="styling.css"
-  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-</head>
-<body>
-  <div style="border-radius: 50; border: 1 px;">
+<div style="border-radius: 50; border: 1 px;">
     <img src=""> <!--border-radius: 50-->
     <a href="">Username Goes Here</a>
     <p>Tweet text goes here.</p>
@@ -15,8 +8,3 @@
       <span class="glyphicon glyphicons-share"></span><p> Direct Message</p>
     </div>
   </div>
-
-  <!--Tweet replies will go here.-->
-
-</body>
-</html>
