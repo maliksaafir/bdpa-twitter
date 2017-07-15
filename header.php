d47058bd964ab55568e9b8d0c2d44eebe2be0b04
christophrrb <christopherballenger@gmail.com>
2017-07-15 11:44:50 -0400

    Ugly header

---
+++ header.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+  <div>
+    <a href="#">Home</a>
+    <a href="#">Profile</a>
+    <a href="#">Messages</a>
+    <form style="float: right">
+      <input type="text" placeholder="Search Users">
+    </form>
+  </div>
+</body>
+</html>
