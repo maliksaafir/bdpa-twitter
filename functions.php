c34cbe1508d912fe05dc5bd03b580a1acc44efff
Malik Saafir <msaafir99@gmail.com>
2017-07-22 12:16:01 -0400

    block and unblock functions in progress

--- functions.php
+++ functions.php
@@ -1,13 +1,31 @@
 <?php
   require_once('connect.php');
 
-  function connection_exists(int $user1, int $user2, $connection) {
+  function connection_exists(int $user1, int $user2, mysqli $connection) {
     $sql = <<<SQL
     SELECT * FROM connections
     WHERE
       follower = $user1
     AND
       followed = $user2
+SQL;
+
+    if ($result = $connection->query($sql)) {
+      if ($row = $result->fetch_object()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+
+  function block_exists(int $user1, int $user2, mysqli $connection) {
+    $sql = <<<SQL
+      SELECT * FROM blocks
+      WHERE
+        blocker = $user1
+      AND
+        blocked = $user2
 SQL;
 
     if ($result = $connection->query($sql)) {
@@ -65,5 +83,40 @@ SQL;
     }
   }
 
-  echo unfollow(1, 1, $conn);
+  function block(int $user1, int $user2, $connection) {
+    if ($user1 == $user2) {
+      return 0;
+    }
+
+    if (block_exists($user1, $user2, $connection)) {
+      return 0;
+    }
+
+    $block_sql = <<<SQL
+      INSERT INTO blocks
+        (blocker, blocked)
+      VALUES
+        ($user1, $user2)
+SQL;
+
+    if (connection_exists($user1, $user2, $connection)) {
+      unfollow($user1, $user2, $connection);
+    }
+
+    if ($connection->query($block_sql)) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  function unblock($user1, $user2, $connection) {
+    if ($user1 == $user2) {
+      return 0;
+    }
+
+    if (!block_exists($user1, $user2, $connection)) {
+      return 0;
+    }
+  }
 ?>
