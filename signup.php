76e38f7df26d929546b8da0032d3a89549920e4c
christophrrb <christopherballenger@gmail.com>
2017-07-22 12:44:56 -0400

    Signup Page

---
+++ signup.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Sign Up - BDPA Twitter</title>
+  </head>
+  <body>
+    <form action="user_created_test.php" method="post">
+      Username: <input type="text" name="username">
+      <br>
+      Password: <input type="password" name="password">
+      <br>
+      Email: <input type="email" name="email">
+      <input type="submit">
+    </form>
+  </body>
+</html>
